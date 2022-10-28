ec166d7d40527cfe6d45d5fbfddabb7a0c9f5c9f
mehnazimu <mehnaz.imu@gmail.com>
2022-10-28 08:44:03 +0600

    data updated

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 
 const courses = require('./data/courses.json');
+const course_details = require('./data/courseDetails.json');
 
 app.get('/', (req, res) => {
     res.send('News API Running');
@@ -13,7 +14,19 @@ app.get('/', (req, res) => {
 
 app.get('/course-categories', (req, res) => {
     res.send(courses);
-})
+});
+app.get('/course-details', (req, res) => {
+    res.send(course_details);
+});
+
+
+app.get('/course_details/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedCourse = course_details.find(c => c.id === id);
+    res.send(selectedCourse);
+});
+
+
 
 app.listen(port, () => {
     console.log('random api is running', port);
